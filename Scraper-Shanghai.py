38da49067682521f1929dacc79b0a83fcc7f73a9
nuaalida <354052374@qq.com>
2015-10-05 00:49:40 +0800

    beta verision.

---
+++ Scraper-Shanghai.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: 骆克云
+# @Date:   2015-10-03 20:47:24
+# @Last Modified by:   骆克云
+# @Last Modified time: 2015-10-04 22:03:13
+
+from PyQt4 import QtCore,QtGui
+from pymongo import MongoClient
+import subprocess
+import os
+
+import Scraper_rc
+
+class ConfigurationPage(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(ConfigurationPage, self).__init__(parent)
+
+        configGroup = QtGui.QGroupBox(u"网站设置")
+
+        #项目名称
+        projectLabel = QtGui.QLabel(u"项目名称:")
+        projectEdit = QtGui.QLineEdit()
+        projectEdit.setText(u"上海市政府信息采购网站爬取")
+        projectEdit.setReadOnly(True)
+
+        #网址
+        serverLabel = QtGui.QLabel(u"网址:")
+        serverCombo = QtGui.QComboBox()
+        serverCombo.addItem("http://www.shzfcg.gov.cn:8090/new_web/cjxx/center_hz_zb.jsp")
+
+        #爬取模块
+        spiderLabel=QtGui.QLabel(u"爬取模块:")
+        spiderEdit=QtGui.QLineEdit()
+        spiderEdit.setText("shzfcgSpider")
+        spiderEdit.setReadOnly(True)
+        
+        #布局设置
+        projectLayout=QtGui.QGridLayout()
+        projectLayout.addWidget(projectLabel,0,0)
+        projectLayout.addWidget(projectEdit,0,1)
+        projectLayout.addWidget(serverLabel,1,0)
+        projectLayout.addWidget(serverCombo,1,1)
+        projectLayout.addWidget(spiderLabel,2,0)
+        projectLayout.addWidget(spiderEdit,2,1)
+
+        configGroup.setLayout(projectLayout)
+
+        #网站分析
+        parameterGroup=QtGui.QGroupBox(u"参数分析-域设置")
+    
+        fieldList= QtGui.QListWidget()
+        urlItem = QtGui.QListWidgetItem(fieldList)
+        urlItem.setText("url")
+        projNameItem=QtGui.QListWidgetItem(fieldList)
+        projNameItem.setText("projName")
+        merchantItem=QtGui.QListWidgetItem(fieldList)
+        merchantItem.setText("merchant")
+        dateItem=QtGui.QListWidgetItem(fieldList)
+        dateItem.setText("date")
+        priceItem=QtGui.QListWidgetItem(fieldList)
+        priceItem.setText("price")
+
+        parameterLayout= QtGui.QVBoxLayout()
+        parameterLayout.addWidget(fieldList)
+        parameterGroup.setLayout(parameterLayout)
+
+        #数据库设置
+        dataBaseGroup=QtGui.QGroupBox(u"MongoDB数据库")
+        staticData=(("MONGODB_SERVER","localhost"),("MONGODB_PORT","27017"),("MONGODB_DB","shzfcg"),("MONGODB_COLLECTION","caigou"),)
+
+        tableWidget = QtGui.QTableWidget(4, 2)
+        headerLabels = (u"MongoDB参数", u"值")
+        tableWidget.setHorizontalHeaderLabels(headerLabels)
+
+        for row,(mongoParameter,mongoValue) in enumerate(staticData):
+            item0=QtGui.QTableWidgetItem(mongoParameter)
+            item1=QtGui.QTableWidgetItem(mongoValue)
+            tableWidget.setItem(row,0,item0)
+            tableWidget.setItem(row,1,item1)
+
+        tableWidget.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        mongoDBLayout=QtGui.QVBoxLayout()
+        mongoDBLayout.addWidget(tableWidget)
+        dataBaseGroup.setLayout(mongoDBLayout)
+
+        #主布局设计
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(configGroup)
+        mainLayout.addWidget(parameterGroup)
+        mainLayout.addWidget(dataBaseGroup)
+        mainLayout.addStretch(1)
+
+        self.setLayout(mainLayout)
+
+
+class CrawlPage(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(CrawlPage, self).__init__(parent)
+        self.success=True
+        #爬虫布局
+        crawlGroup = QtGui.QGroupBox(u"爬虫")
+        startButton=QtGui.QPushButton(u"开始")
+        self.textEdit = QtGui.QTextEdit()
+        self.textEdit.setMaximumHeight(100)
+        self.textEdit.setReadOnly(True)
+
+        startButton.clicked.connect(self.crawl)
+        
+        crawlLayout = QtGui.QVBoxLayout()
+        crawlLayout.addWidget(startButton)
+        crawlLayout.addWidget(self.textEdit)
+        crawlGroup.setLayout(crawlLayout)
+
+        #结果布局
+        resultGroup=QtGui.QGroupBox(u"爬虫结果")
+        self.table=QtGui.QTableWidget(0,4)
+        self.table.setMinimumHeight(450)
+        headerLabels = (u"项目名称", u"成交供应商",u"采购日期",u"成交金额")
+        self.table.setHorizontalHeaderLabels(headerLabels)
+        self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        resultLayout=QtGui.QVBoxLayout()
+        resultLayout.addWidget(self.table)
+        resultGroup.setLayout(resultLayout)
+
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(crawlGroup)
+        mainLayout.addWidget(resultGroup)
+        mainLayout.addSpacing(12)
+        mainLayout.addStretch(1)
+
+        self.showResult(False)
+        
+        self.setLayout(mainLayout)
+
+    def crawl(self):
+        os.chdir("shzfcg/")
+        p=subprocess.Popen("scrapy crawl shzfcgSpider",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        self.textEdit.clear()
+        cursor = self.textEdit.textCursor()
+        for line in p.stdout.readlines():
+            cursor.insertText(line)
+            if line.find("DNS lookup failed"):
+                self.success=False
+        retval=p.wait()
+        if p.returncode!=0:
+            cursor.insertText("error!")
+            return -1
+        self.showResult(self.success==0)
+
+    def showResult(self,isShow):
+        if not isShow:
+            return
+        client=MongoClient()
+        db=client.shzfcg
+        collection=db.caigou
+        self.table.setRowCount(collection.count())
+        row=0
+        for p in collection.find():
+            item0=QtGui.QTableWidgetItem(p.get("projName","-1"))
+            item1=QtGui.QTableWidgetItem(p.get("merchant","-1"))
+            item2=QtGui.QTableWidgetItem(p.get("date","-1"))
+            item3=QtGui.QTableWidgetItem(p.get("price","-1"))
+            self.table.setItem(row,0,item0)
+            self.table.setItem(row,1,item1)
+            self.table.setItem(row,2,item2)
+            self.table.setItem(row,3,item3)
+            row=row+1
+       
+
+
+class QueryPage(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(QueryPage, self).__init__(parent)
+
+        packagesGroup = QtGui.QGroupBox(u"查询")
+
+        merchantLabel = QtGui.QLabel(u"供应商:")
+        merchantEdit = QtGui.QLineEdit()
+
+        projLabel = QtGui.QLabel(u"项目名称:")
+        projEdit = QtGui.QLineEdit()
+
+        self.fromDateEdit = QtGui.QDateEdit()
+        self.fromDateEdit.setDate(QtCore.QDate(2015, 1, 1))
+        self.fromDateEdit.setCalendarPopup(True)
+        fromLabel = QtGui.QLabel(u"时间起始:")
+        fromLabel.setBuddy(self.fromDateEdit)
+        self.toDateEdit = QtGui.QDateEdit()
+        self.toDateEdit.setDate(QtCore.QDate.currentDate())
+        self.toDateEdit.setCalendarPopup(True)
+        toLabel = QtGui.QLabel(u"截至:")
+        toLabel.setBuddy(self.toDateEdit)
+
+        priceLabel=QtGui.QLabel(u"金额:")
+        priceLowSpinBox = QtGui.QSpinBox()
+        priceLowSpinBox.setPrefix(u"金额下限: ")
+        priceLowSpinBox.setSuffix(u" 元")
+        priceLowSpinBox.setSpecialValueText(u"金额下限: 0 元")
+        priceLowSpinBox.setMinimum(0)
+        priceLowSpinBox.setMaximum(1000000)
+        priceLowSpinBox.setSingleStep(100)
+
+
+        priceHighSpinBox = QtGui.QSpinBox()
+        priceHighSpinBox.setPrefix(u"金额上限: ")
+        priceHighSpinBox.setSuffix(u" 元")
+        priceHighSpinBox.setSpecialValueText(u"金额上限: 1000000 元")
+        priceHighSpinBox.setMinimum(0)
+        priceHighSpinBox.setMaximum(1000000)
+        priceHighSpinBox.setSingleStep(100)
+
+
+        startQueryButton = QtGui.QPushButton(u"开始查询")
+        clearQueryButton=QtGui.QPushButton(u"清除结果")
+
+         #结果布局
+        resultGroup=QtGui.QGroupBox(u"爬虫结果")
+        self.table=QtGui.QTableWidget(0,4)
+        self.table.setMinimumHeight(450)
+        headerLabels = (u"项目名称", u"成交供应商",u"采购日期",u"成交金额")
+        self.table.setHorizontalHeaderLabels(headerLabels)
+        self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
+        resultLayout=QtGui.QVBoxLayout()
+        resultLayout.addWidget(self.table)
+        resultGroup.setLayout(resultLayout)
+
+        packagesLayout = QtGui.QGridLayout()
+        packagesLayout.addWidget(merchantLabel, 0, 0)
+        packagesLayout.addWidget(merchantEdit, 0, 1)
+        packagesLayout.addWidget(projLabel, 0, 2)
+        packagesLayout.addWidget(projEdit, 0, 3)
+        packagesLayout.addWidget(fromLabel, 1, 0)
+        packagesLayout.addWidget(self.fromDateEdit, 1, 1, 1, 1)
+        packagesLayout.addWidget(toLabel, 1, 2)
+        packagesLayout.addWidget(self.toDateEdit, 1, 3, 1, 1)
+        packagesLayout.addWidget(priceLabel,2,0)
+        packagesLayout.addWidget(priceLowSpinBox, 2, 1, 1, 1)
+        packagesLayout.addWidget(priceHighSpinBox, 2, 3, 1, 1)
+        packagesLayout.addWidget(startQueryButton,3,0)
+        packagesLayout.addWidget(clearQueryButton,3,1)
+        
+        packagesGroup.setLayout(packagesLayout)
+
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(packagesGroup)
+        mainLayout.addWidget(resultGroup)
+        mainLayout.addSpacing(12)
+        mainLayout.addStretch(1)
+
+        startQueryButton.clicked.connect(self.queryResult)
+        clearQueryButton.clicked.connect(self.clearResult)
+        self.showResult(True)
+        self.setLayout(mainLayout)
+
+    def queryResult(self):
+        pass
+
+    def clearResult(self):
+        self.table.clear()
+
+    def showResult(self,isShow):
+        if not isShow:
+            return
+        client=MongoClient()
+        db=client.shzfcg
+        collection=db.caigou
+        self.table.setRowCount(collection.count())
+        row=0
+        for p in collection.find():
+            item0=QtGui.QTableWidgetItem(p.get("projName","-1"))
+            item1=QtGui.QTableWidgetItem(p.get("merchant","-1"))
+            item2=QtGui.QTableWidgetItem(p.get("date","-1"))
+            item3=QtGui.QTableWidgetItem(p.get("price","-1"))
+            self.table.setItem(row,0,item0)
+            self.table.setItem(row,1,item1)
+            self.table.setItem(row,2,item2)
+            self.table.setItem(row,3,item3)
+            row=row+1
+
+
+class ConfigDialog(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(ConfigDialog, self).__init__(parent)
+        self.setFixedSize(1000,705)
+        self.contentsWidget = QtGui.QListWidget()
+        self.contentsWidget.setViewMode(QtGui.QListView.IconMode)
+        self.contentsWidget.setIconSize(QtCore.QSize(96, 84))
+        self.contentsWidget.setMovement(QtGui.QListView.Static)
+        self.contentsWidget.setMaximumWidth(128)
+        self.contentsWidget.setSpacing(12)
+
+        self.pagesWidget = QtGui.QStackedWidget()
+        self.pagesWidget.addWidget(ConfigurationPage())
+        self.pagesWidget.addWidget(CrawlPage())
+        self.pagesWidget.addWidget(QueryPage())
+
+        closeButton = QtGui.QPushButton(u"关闭")
+
+        self.createIcons()
+        self.contentsWidget.setCurrentRow(1)
+
+        closeButton.clicked.connect(self.close)
+
+        horizontalLayout = QtGui.QHBoxLayout()
+        horizontalLayout.addWidget(self.contentsWidget)
+        horizontalLayout.addWidget(self.pagesWidget, 1)
+
+        buttonsLayout = QtGui.QHBoxLayout()
+        buttonsLayout.addStretch(1)
+        buttonsLayout.addWidget(closeButton)
+
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addLayout(horizontalLayout)
+        mainLayout.addStretch(1)
+        mainLayout.addSpacing(12)
+        mainLayout.addLayout(buttonsLayout)
+
+        self.setLayout(mainLayout)
+
+        self.setWindowTitle(u"上海政府采购网爬虫")
+
+
+    def changePage(self, current, previous):
+        if not current:
+            current = previous
+
+        self.pagesWidget.setCurrentIndex(self.contentsWidget.row(current))
+
+    def createIcons(self):
+        configButton = QtGui.QListWidgetItem(self.contentsWidget)
+        configButton.setIcon(QtGui.QIcon(u':/images/配置.png'))
+        configButton.setText(u"配置")
+        configButton.setTextAlignment(QtCore.Qt.AlignHCenter)
+        configButton.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+
+        updateButton = QtGui.QListWidgetItem(self.contentsWidget)
+        updateButton.setIcon(QtGui.QIcon(u':/images/爬取.png'))
+        updateButton.setText(u"爬取")
+        updateButton.setTextAlignment(QtCore.Qt.AlignHCenter)
+        updateButton.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+
+        queryButton = QtGui.QListWidgetItem(self.contentsWidget)
+        queryButton.setIcon(QtGui.QIcon(u':/images/查询.png'))
+        queryButton.setText(u"查询")
+        queryButton.setTextAlignment(QtCore.Qt.AlignHCenter)
+        queryButton.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+
+        self.contentsWidget.currentItemChanged.connect(self.changePage)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+    dialog = ConfigDialog()
+    sys.exit(dialog.exec_()) 
---
+++ shzfcg/shzfcg/spiders/shzfcgSpider.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+import re
+from scrapy import log, Request
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+
+from shzfcg.items import ShzfcgItem
+
+class shzfcgSpider(CrawlSpider):
+	
+	name = "shzfcgSpider"
+	allowed_domains = ["shzfcg.gov.cn"]
+	start_urls = [
+		"http://www.shzfcg.gov.cn:8090/new_web/cjxx/center_hz_zb.jsp",
+	]
+	link_extractor = {
+    	'content' : SgmlLinkExtractor(allow = 'code=\d+&type=\d+$'),
+    }
+	page = 1
+	
+	def parse(self, response):
+		count = 0
+		
+		for element in response.xpath('//td[@align="right"]/a').extract():
+			if count > 0 :
+				break
+				
+			element = element.encode('utf-8')
+			pos = element.find('下一页')
+			element = element.decode('utf-8')
+			if pos > -1 :
+				count = count+1
+				shzfcgSpider.page = shzfcgSpider.page+1
+				yield Request(url = '%s?page=%d' %(shzfcgSpider.start_urls[0] , shzfcgSpider.page), callback = self.parse)
+
+		for link in self.link_extractor['content'].extract_links(response):
+			yield Request(url = link.url, callback=self.parse_page_content)
+			
+	def parse_page_content(self, response):
+		item = ShzfcgItem()
+
+		tempstr = ''.join(response.xpath('//td[@class="text1"]/strong/text()').extract())
+		if tempstr.find('---') > -1 :
+			item['projName'] = tempstr[tempstr.find('---')+3:] 
+		else :
+			item['projName'] = tempstr[tempstr.find('--')+2:] 
+		
+		for element in response.xpath('//td[@class="text3"]/text()').extract():
+			
+			element = element.encode("utf-8")
+			pos_1_1 = element.find('供应商：') # 成交供应商 和 中标供应商
+			pos_1_2 = element.find('中标单位：')
+			pos = element.find('：')
+			
+			pos_2_1 = element.find('采购日期')
+			pos_2_2 = element.find('评审日期')
+			pos_2_3 = element.find('评标日期')
+			
+			pos_3_1 = element.find('成交金额')
+			pos_3_2 = element.find('中标金额')
+			element = element.decode("utf-8")
+			
+			if pos_1_1 > -1 or pos_1_2 > -1 :
+				
+				pos_1 = max(pos_1_1, pos_1_2)
+				
+				if element.find(',') > -1 :	
+					item['merchant'] = element[pos_1:element.find(',')]
+				
+				else :
+					item['merchant'] = element[pos_1:]
+				
+			if pos_2_1 >=0 or pos_2_2 >=0 or pos_2_3 >=0 :
+				item['date'] = re.compile('\d{4}\D\d+\D\d+').search(element).group()
+				
+			if pos_3_1 > -1 or pos_3_2 > -1 :
+				item['price'] = re.compile('\d+\.?\d*').search(element).group()
+			
+		return item
+
